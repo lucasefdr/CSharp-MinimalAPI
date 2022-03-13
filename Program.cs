f9a673060bf865a893436d8c2e2e27ec8dc69f60
Lucas <lcseferreira@hotmail.com>
2022-03-13 14:05:27 -0300

    Configuração do DbContext e criação do banco

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(); // A connection string esjá está configurada
+
 var app = builder.Build();
 
-app.MapGet("/", () => {
-    var todo = new ToDo(Guid.NewGuid(), "Ir para academia", false);
-    return Results.Ok(todo);
+app.MapGet("v1/todos", (AppDbContext context) =>
+{
+    var todos = context.ToDos.ToList();
+    return Results.Ok(todos);
 });
 
 app.Run();
+ 
